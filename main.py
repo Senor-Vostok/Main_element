d13442e79586b808aa86bd91b98a5468f946c399
Senor_Vostok <tabulgatin1@gmail.com>
2024-05-15 22:09:55 +0500

    checkpoint 2 fixed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+import obb.Constants
 from obb.Handler.Handler import EventHandler
 from obb.Handler.Handler_show import show_menu
 import pygame
@@ -7,13 +8,11 @@ import pygame
 
 if __name__ == '__main__':
     pygame.init()
-    file = open('data/user/information', mode='rt')
-    if file.read() == 'UID':
-        file.close()
-        file = open('data/user/information', mode='w')
-        uid = random.randint(4, 100000000)
-        file.write('0' * (9 - len(str(uid))) + str(uid))
-        file.close()
+    with open('data/user/information', mode='rt') as file:
+        if file.read() == 'UID':
+            uid = random.randint(obb.Constants.MIN_UID, obb.Constants.MAX_UID)
+            with open('data/user/information', mode='w') as file:
+                file.write('0' * (len(str(obb.Constants.MAX_UID)) - len(str(uid))) + str(uid))
     handler = EventHandler()
     show_menu(handler, handler.centre)
     while 1:
